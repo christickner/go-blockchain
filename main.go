fa258b0616dce669f161e17a6ea3db0a44833f5c
Chris Tickner <chris@tickner.io>
2019-02-23 22:07:17 -0500

    main.go now creates a blockchain and writes it to stdout

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"github.com/christickner/go-blockchain/blockchain"
+)
+
+func main() {
+	bc := blockchain.NewBlockchain()
+
+	bc.AddBlock("Send 1 Coin to Chris")
+	bc.AddBlock("Send 3 Coins to Chris")
+	bc.AddBlock("Send 2 Coins From Chris to John")
+
+	for _, block := range bc.Blocks() {
+		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
