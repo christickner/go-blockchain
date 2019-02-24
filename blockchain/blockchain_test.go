3e8fc308de4d3636faa62d701b205a1e0c3a5687
Chris Tickner <chris@tickner.io>
2019-02-23 21:50:12 -0500

    test the Blockchain.AddBlock method

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -13,3 +13,17 @@ func TestNewBlockchain(t *testing.T) {
 		t.Error("Unexpected data in the genesis Block")
 	}
 }
+
+func TestAddBlock(t *testing.T) {
+	bc := NewBlockchain()
+
+	bc.AddBlock("the second block")
+
+	if len(bc.blocks) != 2 {
+		t.Errorf("Expected 2 Blocks in the Blockchain, got: %d", len(bc.blocks))
+	}
+
+	if string(bc.blocks[1].Data) != "the second block" {
+		t.Errorf("Unexpected Block data, got: %s", string(bc.blocks[1].Data))
+	}
+}
