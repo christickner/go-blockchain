3222e24683704e5b315224e457d1474e4bd25742
Chris Tickner <chris@tickner.io>
2019-02-23 21:26:09 -0500

    add better block_test.go error messages

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -9,18 +9,18 @@ func TestNewBlock(t *testing.T) {
 	b := NewBlock("some data", []byte{'1', '2'})
 
 	if string(b.Data) != "some data" {
-		t.Fail()
+		t.Error("data not in block")
 	}
 
 	if b.Timestamp == 0 {
-		t.Fail()
+		t.Error("timestamp was not set")
 	}
 
 	if b.PrevBlockHash[0] != '1' || b.PrevBlockHash[1] != '2' || len(b.PrevBlockHash) != 2 {
-		t.Fail()
+		t.Errorf("PrevBlockHash is not what we passed in, %v", b.PrevBlockHash)
 	}
 
 	if len(b.Hash) == 0 {
-		t.Fail()
+		t.Error("block hash was not set")
 	}
 }
