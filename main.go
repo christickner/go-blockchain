ea054ec9ae9f78ad7d1fc5678a192152010fadcf
Chris Tickner <chris@tickner.io>
2019-02-28 00:28:21 -0500

    main.go: now all blocks are mined and contain the valid golden nonce

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	for _, block := range bc.Blocks() {
 		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
 		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Println()
 	}
