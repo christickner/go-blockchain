c439d30155c954a16953cdd5e54512a2f3f3816c
Chris Tickner <chris@tickner.io>
2019-02-28 00:27:36 -0500

    block: add nonce, use pow to mine new blocks and find the golden nonce

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,6 @@
 package blockchain
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
@@ -12,14 +9,7 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
+	Nonce         int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
@@ -28,8 +18,14 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 		[]byte(data),
 		prevBlockHash,
 		[]byte{},
+		0,
 	}
-	block.SetHash()
+
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
 }
